b21926aef35ce9512f6a9f07fc9abdd7034e1be4
agarwh1 <105258192+agarwh1@users.noreply.github.com>
2023-10-22 21:11:31 +0100

    Add files via upload

---
+++ pendingcode.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author school
+ */
+public class pendingcode {
+    String pendname;
+    String pendmaterial;
+    String pendcolor;
+    String pendQTY;
+    
+    /**
+     *
+     * @param pn
+     * @param pm
+     * @param pc
+     * @param qty
+     */
+    public pendingcode(String pn, String pm, String pc, String qty){
+        this.pendname = pn;
+        this.pendmaterial = pm;
+        this.pendcolor = pc;
+        this.pendQTY = qty;
+      
+    }
+    
+}
